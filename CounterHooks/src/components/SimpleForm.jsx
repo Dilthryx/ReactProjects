ea022b50c9005dd49dc7be10ad4d9130a9cb4ce6
dilthryx <emejia11193@gmail.com>
2025-02-24 20:47:26 -0600

    Refactor SimpleForm component to add additional form fields and submit functionality

--- CounterHooks/src/components/SimpleForm.jsx
+++ CounterHooks/src/components/SimpleForm.jsx
@@ -4,10 +4,16 @@ import { Message } from './Message';
 export const SimpleForm = () => {
   const [formState, setFormState] = useState({
     username: 'Dilthryx',
-    email: 'emejia11193@gmail.com'
+    email: 'emejia11193@gmail.com',
+    matricula: 'A01412143',
+    apellidos: 'Mejia',
+    Edad: '21',
+    Universidad: 'ITESM',
+    Carrera: 'ITC'
+    
   });
 
-  const { username, email } = formState;
+  const { username, email, matricula, apellidos, Edad, Universidad, Carrera } = formState;
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -27,27 +33,75 @@ export const SimpleForm = () => {
     // console.log('email changed!');
   }, [email]);
 
-  return (
+const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+};
+
+return (
     <>
-      <h1>Formulario Simple</h1>
-      <hr />
-      <input 
-        type="text" 
-        className="form-control" 
-        placeholder="Username" 
-        name="username"
-        value={username}
-        onChange={onInputChange}
-      />
-      <input 
-        type="email" 
-        className="form-control mt-2" 
-        placeholder="jorge@gmail.com" 
-        name="email"
-        value={email}
-        onChange={onInputChange}
-      />
-      {username === 'Dilthryx' && <Message />}
+        <h1>Formulario Simple</h1>
+        <hr />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Username" 
+            name="username"
+            value={username}
+            onChange={onInputChange}
+        />
+        <input 
+            type="email" 
+            className="form-control mt-2" 
+            placeholder="example@gmail.com" 
+            name="email"
+            value={email}
+            onChange={onInputChange}
+        />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Matricula" 
+            name="matricula"
+            value={matricula}
+            onChange={onInputChange}
+        />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Apellidos" 
+            name="apellidos"
+            value={apellidos}
+            onChange={onInputChange}
+        />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Edad" 
+            name="Edad"
+            value={Edad}
+            onChange={onInputChange}
+        />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Universidad" 
+            name="Universidad"
+            value={Universidad}
+            onChange={onInputChange}
+        />
+        <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Carrera" 
+            name="Carrera"
+            value={Carrera}
+            onChange={onInputChange}
+        />
+
+        {username === 'Dilthryx' && <Message />}
+
+        <button className="btn btn-primary mt-2" onClick={onSubmit}>Submit</button>
     </>
-  );
+);
 };
